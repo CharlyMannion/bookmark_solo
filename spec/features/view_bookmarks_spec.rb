28cafb5808d87e18023e347da3d422d20f7c023b
Charlotte Mannion <charlottemannion100@gmail.com>
2020-02-10 15:28:32 +0000

    script cleans the database before each test. Sense checked using tableplus"

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,5 +1,12 @@
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('https://www.picturehouses.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://loveinternationalfestival.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.bbc.co.uk/weather/2641776');")
+
     visit('/bookmarks')
 
     expect(page).to have_content("https://www.picturehouses.com")
