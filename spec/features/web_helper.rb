2f675e09bfd061352d6898aa9dbacbd6519af70e
Charlotte Mannion <charlottemannion100@gmail.com>
2020-02-11 12:34:29 +0000

    refactors web helper to use logic in the model

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,8 +1,5 @@
 def add_test_data
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-
-  # Add the test data
-  connection.exec("INSERT INTO bookmarks (url) VALUES ('https://www.picturehouses.com');")
-  connection.exec("INSERT INTO bookmarks (url) VALUES('https://loveinternationalfestival.com');")
-  connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.bbc.co.uk/weather/2641776');")
+  Bookmark.create(url: 'https://www.picturehouses.com')
+  Bookmark.create(url: 'https://loveinternationalfestival.com')
+  Bookmark.create(url: 'https://www.bbc.co.uk/weather/2641776')
 end
